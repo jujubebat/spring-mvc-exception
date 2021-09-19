f5c11dffe997fb30c4cc0c7ce1189cf4068cade9
Byung Wook Park <bbwwpark@naver.com>
2021-09-19 16:55:37 +0900

    feat: 필터 DispatchType 학습 완료

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR); // 이 필터는 DispatcherType 타입이 REQUEST, ERROR 경우에만 호출
+        return filterRegistrationBean;
+    }
+
+}
