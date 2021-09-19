afa91a8b31aa7b811c3387f9cd9c0360b5df211d
Byung Wook Park <bbwwpark@naver.com>
2021-09-19 17:02:48 +0900

    feat: 인터셉터 적용함 - excludePathPatterns를 사용하여 오류 페이지 요청에 대해서는 인터셉터 적용 안 하도록 함

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,23 +1,36 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean logFilter(){
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns(
+                "/css/**", "/*.ico"
+                , "/error", "/error-page/**" //오류 페이지 경로 제외! (오류 페이지 요청에 대해서는 인터셉터 적용 x)
+            );
+    }
+
+    // @Bean
+    public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
-        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR); // 이 필터는 DispatcherType 타입이 REQUEST, ERROR 경우에만 호출
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST,
+            DispatcherType.ERROR); // 이 필터는 DispatcherType 타입이 REQUEST, ERROR 경우에만 호출
         return filterRegistrationBean;
     }
 
